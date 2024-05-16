e3ac30214b2f9af658fc596749223ecaf96ca2be
Jade Ellis <jade@ellis.link>
2024-05-16 14:21:08 +0100

    Create open-on-wayback.js

---
+++ bookmarklets/open-on-wayback.js
@@ -0,0 +1,7 @@
+// ==Bookmarklet==
+// @name Open URL on Wayback Machine
+// ==/Bookmarklet==
+var url = document.URL
+
+window.location.href = "http://web.archive.org/web/*/"
+                        + url
